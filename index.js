8d677dd9e45db41880fec0d95a42bede571f3f14
Alex Glover <alex@circulation.com>
2020-10-14 10:49:38 -0400

    smooth scroll

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const onEventClick = (e) => {
+let isMobile
+
+const onHomeNavClick = (e) => {
   e.preventDefault()
   $('.home-search-nav-link.active').removeClass('active')
   e.target.classList.add('active')
@@ -12,19 +14,58 @@ const onEventClick = (e) => {
 let carouselScrollVal = 0
 const onCarouselScroll = (e) => {
   const ofFive = Math.floor(((e.target.scrollLeft + 130) * 5) / e.target.scrollWidth)
-  
-  if (ofFive != carouselScrollVal) {
+  const fullLineWidth = $('#carousel-line-this-week').width()
+  if (isMobile && ofFive != carouselScrollVal) {
     carouselScrollVal = ofFive
     $('#carousel-line-active-this-week').animate({
-      'margin-left': ($('#carousel-line-this-week').width() / 5) * ofFive
+      'margin-left': (fullLineWidth / 5) * ofFive
     }, 200)
+  } else {
+    const activeLineWidth = $('#carousel-line-active-this-week').width()
+    const totalDistance = fullLineWidth - activeLineWidth
+
+    const scrollTo = (totalDistance * e.target.scrollLeft) / (e.target.scrollWidth - window.innerWidth)
+    $('#carousel-line-active-this-week').css('margin-left', scrollTo)
   }
 }
 
+const setScrollWidth = () => {
+  const widthCalc = 112 - (100 * (($('#carousel-active-this-week')[0].scrollWidth) - $('#carousel-line-this-week').width())) / (window.innerWidth)
+  const calc = ((40 * widthCalc) / 100) + 60
+
+  $('#carousel-line-active-this-week').css('width', `${calc}%`)
+}
+
+const checkMobile = () => {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+    isMobile = true
+    $('.carousel').each(function() {
+      $(this).addClass('snap')
+    })
+  } else {
+    // false for not mobile device
+    isMobile = false
+    $('.carousel').each(function() {
+      $(this).removeClass('snap')
+    })
+    setScrollWidth()
+  }
+}
+
+const onWindowResize = () => {
+  setScrollWidth()
+}
+
+
 $(document).ready(() => {
+  checkMobile()
+
   $('.home-search-nav-link').each(function() {
-    $(this).on('click', onEventClick)
+    $(this).on('click', onHomeNavClick)
   })
 
   $('#carousel-active-this-week').on('scroll', onCarouselScroll)
+
+  window.addEventListener('resize', onWindowResize);
 })
